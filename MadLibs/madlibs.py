af8acba58f68a189ecca4ca36343f51b3172850d
Travis Morse <travis503@gmail.com>
2022-04-12 12:45:52 -0500

    Madlibs

---
+++ MadLibs/madlibs.py
@@ -0,0 +1,9 @@
+def MadLibs():
+  noun1 = input('Enter a noun: ')
+  noun2 = input('Enter another noun: ')
+  adjective1 = input('Enter an adjective: ')
+  adjective2 = input('Enter another adjective: ')
+  verb1 = input('Enter a verb: ')
+  verb2 = input('Enter another verb: ')
+  print('Bob went to the store to buy a ' + noun1 + '. While he was there, he saw a ' + noun2 + ' for sale! "What a ' + adjective1 + ' bargain!", he said. He ' + verb1 + 'ed the ' + noun2 + ' into his shopping cart and ' + verb2 + "ed to the cashier. Isn't that a " + adjective2 + ' story?')
+MadLibs()
