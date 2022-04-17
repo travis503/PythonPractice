dda399d29ff9e70d91a4d500903ff2747818ea09
Travis Morse <travis503@gmail.com>
2022-04-17 00:50:41 -0500

    Trimmed extraneous values from Ship_Room class

--- AdventureGame/adventure_game.py
+++ AdventureGame/adventure_game.py
@@ -1,7 +1,7 @@
 def adventure_game():
 
   class ShipRoom:
-    def __init__(self, name, issue, issue_broken, issue_fixed, issue_is_fixed, left, right, forward, behind, left_room, right_room, forward_room, behind_room):
+    def __init__(self, name, issue, issue_broken, issue_fixed, issue_is_fixed, left, right, forward, behind):
       self.name = name
       self.issue = issue
       self.issue_broken = issue_broken
@@ -11,16 +11,12 @@ def adventure_game():
       self.right = right
       self.forward = forward
       self.behind = behind
-      self.left_room = left_room
-      self.right_room = right_room
-      self.forward_room = forward_room
-      self.behind_room = behind_room
 
-  engine_room = ShipRoom('the engine room', 'the engines', 'The engines are silent.', 'The engines are humming away.', False, False, False, True, False, None, None, 'the medbay', None)
-  medbay_room = ShipRoom('the medbay', 'the bioscanner', 'The bioscanner is flashing red.', 'The bioscanner is indicating green.', False, True, True, True, True, 'the weapons room', 'the shields room', 'the navigation room', 'the engine room')
-  weapons_room = ShipRoom('the weapons room', 'the lasers', 'The lasers are misaligned.', 'The lasers are perfectly aligned.', False, False, False, False, True, None, None, None, 'the medbay')
-  shields_room = ShipRoom('the shields room', 'the shields', 'The shields are flickering.', 'The engines are holding strong.', False, False, False, False, True, None, None, None, 'the medbay')
-  navigation_room = ShipRoom('the navigation room', 'the starmap', 'The starmap is faded.', 'The starmap is nice and vivid.', False, False, False, False, True, None, None, None, 'the medbay')
+  engine_room = ShipRoom('the engine room', 'the engines', 'The engines are silent.', 'The engines are humming away.', False, False, False, True, False)
+  medbay_room = ShipRoom('the medbay', 'the bioscanner', 'The bioscanner is flashing red.', 'The bioscanner is indicating green.', False, True, True, True, True)
+  weapons_room = ShipRoom('the weapons room', 'the lasers', 'The lasers are misaligned.', 'The lasers are perfectly aligned.', False, False, False, False, True)
+  shields_room = ShipRoom('the shields room', 'the shields', 'The shields are flickering.', 'The engines are holding strong.', False, False, False, False, True)
+  navigation_room = ShipRoom('the navigation room', 'the starmap', 'The starmap is faded.', 'The starmap is nice and vivid.', False, False, False, False, True)
 
   engine_room.forward = medbay_room
   medbay_room.forward = navigation_room
@@ -48,13 +44,13 @@ def adventure_game():
     else:
       print(room.issue_broken)
     if (room.left):
-      print('To the left is the ' + room.left_room + ".")
+      print('To the left is the ' + room.left.name + ".")
     if (room.right):
-      print('To the right is the ' + room.right_room + ".")
+      print('To the right is the ' + room.right.name + ".")
     if (room.forward):
-      print('Ahead of you is the ' + room.forward_room + ".")
+      print('Ahead of you is the ' + room.forward.name + ".")
     if (room.behind):
-      print('Behind you is the ' + room.behind_room + ".")
+      print('Behind you is the ' + room.behind.name + ".")
     while (not(user_choice_valid)):
       user_choice = input('What would you like to do? ')
       user_choice = user_choice.lower()
@@ -100,8 +96,7 @@ def adventure_game():
   if (victory_conditions_met == 6):
     print('The imposters win!')
     return
-  # if (victory_conditions_met == 5):
-  print("The ship is repaird, and you're ready to blast off. You win!")
+  print("The ship is repaired, and you're ready to blast off. You win!")
 
 
 
